63528d81239e5ab396b122d36a90b9edcbc07b29
Joshua Alletto <joshalletto@gmail.com>
2019-01-21 15:41:15 -0600

    add apple_spec mamba test.

--- apple_spec.py
+++ apple_spec.py
@@ -1,7 +1,10 @@
-import unittest
-from apple_tree import Apple
+from apple_tree import Apple 
+from mamba import description, context, it
+from expects import expect, equal, be_above_or_equal
 
-class TestAppleTrees(unittest.TestCase):
 
-  def setUp(self):
-    self.apple = Apple()
+with description('Apple') as self:
+  with it('has a diameter greater than 0'):
+    apple = Apple()
+    expect(apple.diameter).to(be_above_or_equal(1))
+  
