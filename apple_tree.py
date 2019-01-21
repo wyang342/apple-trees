2b165916e28530a4a0969a9406af2b8b4fcd2bbf
Jonathan Young <jonyoungg@gmail.com>
2019-01-21 13:58:14 -0600

    working through apple tree implementation

--- apple_tree.py
+++ apple_tree.py
@@ -1,25 +1,33 @@
+import random
+import itertools
+import random
 
 class Apple:
-  def __init__(self,diameter):
+  def __init__(self):
     pass
 
 class AppleTree:
 
   def __init__(self):
-    pass
-
-  def is_mature(self):
-    pass
+      self.age = 0
+      self.height = 0
+      self.death_age = 100
+      self.fruit_bearing_age = 3
+      self.apples = []
   
   def age_tree(self):
-    pass
+      self.age += 1
+      self.height += random.randint(1,7)
+      if self.age >= self.fruit_bearing_age:
+          for _ in itertools.repeat(None, 10):
+              self.apples.append(Apple(random.randint(0,10)))
    
   def is_dead(self):
-    pass
+      # embed()
+      return self.age >= self.death_age
     
   def any_apples(self):
-    # If any apples are present, true. If no apples, false
-    pass
+      return len(self.apples) > 0
 
   def pick_an_apple(self):
     raise Exception('This tree has no apples')
