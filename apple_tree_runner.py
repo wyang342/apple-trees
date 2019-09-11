d3161c6a3b982aa747123dcec47aaf80981cef0d
Mic-J-Lee <mleenavy@gmail.com>
2019-09-11 13:10:19 -0500

    Update apple_tree_runner.py

--- apple_tree_runner.py
+++ apple_tree_runner.py
@@ -2,28 +2,24 @@ from apple_tree import AppleTree
 
 tree = AppleTree()
 
-
 while tree.any_apples() == False:
-  tree.age_tree()
+    tree.age_tree()
 
 print(f"My apple tree is producing apples at age {tree.age} and is {tree.height} feet tall")
 
 while tree.is_dead() == False: 
-  apple_basket = []
+    apple_basket = []
 
-  while tree.any_apples(): 
-    apple_basket.append(tree.pick_an_apple())
- 
+    while tree.any_apples(): 
+        apple_basket.append(tree.pick_an_apple())
 
-  avg_diameter = None # change this so it is the calculated avg diameter of all apples in the basket. 
+    avg_diameter = None # change this so it is the calculated avg diameter of all apples in the basket. 
 
-  print(f"Year {tree.age} Report")
-  print(f"Tree height: {tree.height} feet")
-  print(f"Harvest:     {len(apple_basket)} apples with an average diameter of {avg_diameter} inches")
-  
+    print(f"Year {tree.age} Report")
+    print(f"Tree height: {tree.height} feet")
+    print(f"Harvest:     {len(apple_basket)} apples with an average diameter of {avg_diameter} inches")
 
-  # Age the tree another year
-  tree.age_tree()
-
+    # Age the tree another year
+    tree.age_tree()
 
 print(f"The tree is now dead at age {tree.age}! Time to plant a new one!")
